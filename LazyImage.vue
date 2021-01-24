563ee556e534ace467c7376e3391cc39bb269569
a-salimkhanov <61108267+a-salimkhanov@users.noreply.github.com>
2021-01-24 06:56:04 +0300

    Create

---
+++ LazyImage.vue
@@ -0,0 +1,38 @@
+<template>
+  <span class=“relative-position” :class="spanClass" :style="spanStyle" v-intersection.once="onIntersection">
+    <transition name="q-transition--scale">
+      <img
+        v-if="show"
+        v-show="loaded"
+        @load="loaded=true"
+        :src="src"
+        :title="title"
+        :alt="title"
+        :class="imgClass"
+        :style="imgStyle" />
+    </transition>
+  </span>
+</template>
+
+<script>
+export default {
+  name: 'LazyImg',
+  props: ['title', 'src', 'imgStyle', 'imgClass', 'spanClass', 'spanStyle'],
+  data () {
+    return {
+      show: false,
+      loaded: false
+    }
+  },
+  methods: {
+    onIntersection (entry) {
+      if (entry.isIntersecting) {
+        this.show = true
+      }
+    }
+  }
+}
+</script>
+
+<style lang=“scss” scoped>
+</style>
